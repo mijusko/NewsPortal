10ca161e310fa378d216db1d1b8957fcabc3879a
mijusko <mihajlo00kg@gmail.com>
2024-02-12 18:52:18 +0100

    commit_2

--- news-portal/app/includes/database.inc.php
+++ news-portal/app/includes/database.inc.php
@@ -3,16 +3,18 @@
 // Development Connection
 // Server name or IP Address
 $host = "localhost:3307";
-
+//$host = "db";
 
 // MySQL Username
 $user = "root";
+//$user = "docker";
 
 // MySQL Password
 $pass = "";
+//$pass = "root";
 
 // Default Database name
-$db = "db_news";
+$db = "db_news_rezerva_2";
 
 // Creating a connection to the DataBase
 $con = mysqli_connect($host,$user,$pass,$db);
